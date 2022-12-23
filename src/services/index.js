e367bc39075310289680f4403741dbea62f4a813
tseteespadas <tseteespadas@gmail.com>
2022-12-22 21:20:52 -0300

    fix: fixed api baseURL

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://terreiro-sete-espadas-api.ondigitalocean.app/",
+  baseURL: "https://terreiro-sete-espadas-api-k2nnm.ondigitalocean.app/",
 });

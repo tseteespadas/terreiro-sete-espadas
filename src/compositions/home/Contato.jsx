b1a5fee07b8064e7739f17a65d0b9a356ac90336
Giselle <giselle.costa1409@hotmail.com>
2026-03-26 12:23:23 -0300

    fix: alterando endereço do terreiro

--- src/compositions/home/Contato.jsx
+++ src/compositions/home/Contato.jsx
@@ -79,13 +79,13 @@ export default function Contato() {
             </span>
           </SubtitleWithIcon>
           <p>
-            Rua Engenheiro Reynaldo Cajado, nº 84
+            Rua Dr. Costa Valente, 232
             <br />
-            CEP: 03061-030 - Tatuapé/SP
+            Brás, São Paulo - SP, 03052-000
           </p>
           <iframe
-            title="Rua Engenheiro Reynaldo Cajado, nº 84 - CEP: 03061-030 - Tatuapé/SP"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.899186995021!2d-46.585879685189006!3d-23.53612808469541!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5eda09d765f5%3A0xec620d8fc24c4fc7!2sRua%20Engenheiro%20Reynaldo%20Cajado%2C%2084%20-%20Tatuap%C3%A9%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003061-030!5e0!3m2!1sen!2sbr!4v1622383599162!5m2!1sen!2sbr"
+            title="Rua Dr. Costa Valente, 232 - Brás, São Paulo - SP, 03052-000"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.767323857004!2d-46.612922888568214!3d-23.54086966075238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce592060548b99%3A0x234c73b21c943c7d!2sR.%20Dr.%20Costa%20Valente%2C%20232%20-%20Br%C3%A1s%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2003052-000!5e0!3m2!1spt-BR!2sbr!4v1774538477974!5m2!1spt-BR!2sbr"
             style={{
               border: 0,
               width: "100%",
